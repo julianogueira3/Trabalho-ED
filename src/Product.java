670253a36bba8adc33016ee34d21fc09687b96ea
_julianogueira3 <julia.mjn@icloud.com>
2023-05-13 13:12:28 -0300

    inicio do trabalho

---
+++ src/Product.java
@@ -0,0 +1,14 @@
+public class Product {
+    int code;
+    String name;
+    int quantity;
+    double price;
+
+    public Product(int code, String name, int quantity, double price) {
+        this.code = code;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
+
---
+++ src/TreeNode.java
@@ -0,0 +1,11 @@
+public class TreeNode {
+    Product product;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(Product product) {
+        this.product = product;
+        left = null;
+        right = null;
+    }
+}
