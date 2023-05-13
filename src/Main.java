3415ed130b03cbb6c87161d97a73bb5d982608b1
_julianogueira3 <julia.mjn@icloud.com>
2023-05-13 13:25:11 -0300

    Main e teste

---
+++ src/Main.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ProductTree productTree = new ProductTree();
+
+        boolean running = true;
+        while (running) {
+            System.out.println("===== Gerenciamento de Estoque =====");
+            System.out.println("1. Adicionar produto");
+            System.out.println("2. Remover produto");
+            System.out.println("3. Atualizar quantidade em estoque");
+            System.out.println("4. Exibir estoque");
+            System.out.println("5. Sair");
+            System.out.print("Escolha uma opção: ");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    System.out.println("===== Adicionar Produto =====");
+                    System.out.print("Código: ");
+                    int code = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Nome: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Quantidade: ");
+                    int quantity = scanner.nextInt();
+                    System.out.print("Preço: ");
+                    double price = scanner.nextDouble();
+
+                    Product product = new Product(code, name, quantity, price);
+                    productTree.addProduct(product);
+                    System.out.println("Produto adicionado ao estoque.");
+                    break;
+                case 2:
+                    System.out.println("===== Remover Produto =====");
+                    System.out.print("Código do produto a ser removido: ");
+                    int removeCode = scanner.nextInt();
+                    productTree.removeProduct(removeCode);
+                    System.out.println("Produto removido do estoque.");
+                    break;
+                case 3:
+                    System.out.println("===== Atualizar Quantidade em Estoque =====");
+                    System.out.print("Código do produto: ");
+                    int updateCode = scanner.nextInt();
+                    System.out.print("Nova quantidade: ");
+                    int newQuantity = scanner.nextInt();
+                    productTree.updateQuantity(updateCode, newQuantity);
+                    System.out.println("Quantidade em estoque atualizada.");
+                    break;
+                case 4:
+                    System.out.println("===== Exibir Estoque =====");
+                    displayStock(productTree.root);
+                    break;
+                case 5:
+                    running = false;
+                    System.out.println("Saindo do programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, tente novamente.");
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+
+    private static void displayStock(TreeNode root) {
+        if (root != null) {
+            displayStock(root.left);
+            Product product = root.product;
+            System.out.println("Código: " + product.code);
+            System.out.println("Nome: " + product.name);
+            System.out.println("Quantidade: " + product.quantity);
+            System.out.println("Preço: " + product.price);
+            System.out.println("--------------------");
+            displayStock(root.right);
+        }
+    }
+}
--- src/ProductTree.java
+++ src/ProductTree.java
@@ -1,2 +1,79 @@
 public class ProductTree {
+    TreeNode root;
+
+    public ProductTree() {
+        root = null;
+    }
+
+    public void addProduct(Product product) {
+        root = addProductRec(root, product);
+    }
+
+    private TreeNode addProductRec(TreeNode root, Product product) {
+        if (root == null) {
+            return new TreeNode(product);
+        }
+
+        if (product.code < root.product.code) {
+            root.left = addProductRec(root.left, product);
+        } else if (product.code > root.product.code) {
+            root.right = addProductRec(root.right, product);
+        }
+
+        return root;
+    }
+
+    public void removeProduct(int code) {
+        root = removeProductRec(root, code);
+    }
+
+    private TreeNode removeProductRec(TreeNode root, int code) {
+        if (root == null) {
+            return null;
+        }
+
+        if (code < root.product.code) {
+            root.left = removeProductRec(root.left, code);
+        } else if (code > root.product.code) {
+            root.right = removeProductRec(root.right, code);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            root.product = findMin(root.right).product;
+            root.right = removeProductRec(root.right, root.product.code);
+        }
+
+        return root;
+    }
+
+    public void updateQuantity(int code, int quantity) {
+        TreeNode node = findProductNode(root, code);
+        if (node != null) {
+            node.product.quantity = quantity;
+        }
+    }
+
+    private TreeNode findProductNode(TreeNode root, int code) {
+        if (root == null || root.product.code == code) {
+            return root;
+        }
+
+        if (code < root.product.code) {
+            return findProductNode(root.left, code);
+        }
+
+        return findProductNode(root.right, code);
+    }
+
+    private TreeNode findMin(TreeNode root) {
+        if (root.left != null) {
+            return findMin(root.left);
+        }
+
+        return root;
+    }
 }
